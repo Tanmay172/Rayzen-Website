46b152a9880b4bd026a713071262c8a22ca75d5a
tanmay <tanmaykanwar91@gmail.com>
2022-02-10 13:55:55 +0530

    Rayzen Website

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <>
+    <h1>Place Order At Us</h1>
+  </>, 
+  document.getElementById('Ecommerce-page')
+);
